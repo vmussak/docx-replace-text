b4fe8c7f7beda86a29bb34f8d0cf5b51b1ec60d5
Vinicius Mussak <vinicius.mussak@outlook.com>
2019-07-07 16:24:57 -0300

    Add project files.

---
+++ DocxReplaceExample/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+using Xceed.Words.NET;
+
+namespace ReplaceDocxExample
+{
+    class Program
+    {
+        private static readonly string caminho = @"C:\Users\Vinicius Mussak\Desktop\DocxReplaceExample\DocxReplaceExample\";
+
+        static void Main(string[] args)
+        {
+            Replace();
+
+            Console.WriteLine("Foi :)");
+
+            Console.ReadKey();
+        }
+
+        static void Replace()
+        {
+            using (var documento = DocX.Load(caminho + "documento.docx"))
+            {
+                documento.ReplaceText("#nome", "Vinicius Mussak");
+
+                string mes = DateTime.Now.ToString("MMMM", CultureInfo.CreateSpecificCulture("pt-br"));
+                documento.ReplaceText("#mes", mes);
+
+                documento.SaveAs(caminho + "novo-documento.docx");
+            }
+        }
+
+    }
+}
